16d2b6559e57f67ccdd263fc3d79a9efe6d98985
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2024-01-03 12:18:49 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useScreenSize from "./useScreenSize";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const screenSize = useScreenSize();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Screen Size Detection</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <strong>Width:</strong>
+            </td>
+            <td>{screenSize.width}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Height:</strong>
+            </td>
+            <td>{screenSize.height}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
